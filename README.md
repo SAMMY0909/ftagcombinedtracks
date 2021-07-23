1cf4ad96039123cc6352947ac55565996fd37e5d
Soumyananda Goswami <sgoswami@cern.ch>
2021-07-23 09:52:34 +0200

    Removed folder from repository

---
+++ README.md
@@ -0,0 +1,19 @@
+```
+setupATLAS --quiet
+lsetup git
+git clone https://:@gitlab.cern.ch:8443/atlas-flavor-tagging-tools/FlavourTagPerformanceFramework.git
+cd FlavourTagPerformanceFramework
+git checkout freshstart_r22
+git atlas init-workdir  https://:@gitlab.cern.ch:8443/anburger/athena.git
+mv btagAnalysis athena/
+cd athena
+git checkout release/22.6.3
+#git atlas addpkg DerivationFrameworkExamples
+git atlas addpkg BTagging
+#git atlas addpkg JetTagTools
+cd ..
+\cp -r setuppatch/athena .
+\cp -r combinedpatch/athena . 
+\cp -r combinedpatch/scripts . 
+source scripts/setup.sh
+```
---
+++ combinedpatch/athena/PhysicsAnalysis/JetTagging/JetTagAlgs/BTagging/python/BTagTrackAugmenterAlgConfig.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+
+from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+from AthenaConfiguration.ComponentFactory import CompFactory
+from JetTagTools.BTagTrackToVertexIPEstimatorConfig import BTagTrackToVertexIPEstimatorCfg
+from TrkConfig.AtlasExtrapolatorConfig import AtlasExtrapolatorCfg
+
+Analysis__BTagTrackAugmenterAlg=CompFactory.Analysis.BTagTrackAugmenterAlg
+
+def BTagTrackAugmenterAlgCfg(ConfigFlags, TrackCollection = 'InDetCombinedTrackParticles', PrimaryVertexCollectionName = 'PrimaryVertices', **options):
+    """Adds a SecVtxTool instance and registers it.
+
+    input: name:               The tool's name.
+           options:            Python dictionary of options to be passed to the alg.
+    output: The CA."""
+
+    acc = ComponentAccumulator()
+    # Minimal configuration
+    # @TODO why is options re-initialised to an empty dict ?
+    options = {}
+    options['name'] = ('BTagTrackAugmenter').lower()
+    options['TrackContainer'] = TrackCollection
+    options['PrimaryVertexContainer'] = PrimaryVertexCollectionName
+    if 'TrackToVertexIPEstimator' not in  options :
+        options.setdefault('TrackToVertexIPEstimator',acc.popToolsAndMerge(BTagTrackToVertexIPEstimatorCfg(ConfigFlags, 'TrkToVxIPEstimator') ))
+    if 'Extrapolator' not in options :
+        exrtrapolator_acc = AtlasExtrapolatorCfg(ConfigFlags)
+        options.setdefault('Extrapolator', acc.popToolsAndMerge(exrtrapolator_acc))
+
+    # -- create the track augmenter algorithm
+    acc.addEventAlgo(Analysis__BTagTrackAugmenterAlg(**options))
+
+    return acc
